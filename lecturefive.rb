bfbc2dce62a6a3681b1ad768f03b03e5f1938df2
irhabano234 <irha.bano@gmail.com>
2018-09-03 05:46:21 +0000

    automatically backed up by learn

---
+++ lecturefive.rb
@@ -0,0 +1,14 @@
+loop do
+  puts "Now watch me whip"
+  puts "Watch me nae nae"
+  puts "Now watch me whip, whip"
+  puts "Now watch me nae nae"
+  puts "Ooooooh watch me, watch me"
+  puts "Ooooooh watch me, watch me"
+  puts "Ooooooh watch me, watch me"
+  break
+end
+
+puts "Do the stanky leg"
+puts "Do the Stanky Leg"
+exit
---
+++ sandbox.rb
@@ -0,0 +1,6 @@
+your_name = "MN"
+
+def say_hello
+  puts "Hello# {your_name}"
+end
+say_hello
